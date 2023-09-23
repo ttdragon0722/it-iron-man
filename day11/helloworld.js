343c9deccfd395edb8872e050e934f75a0c24385
ttdragon <a0909956502@gmail.com>
2023-09-23 10:36:02 +0800

    day8

---
+++ day11/helloworld.js
@@ -0,0 +1,50 @@
+// console.log("hello world!!");
+
+array1 = [];
+array2 = [1,2,3];
+array3 = ["rex","vscore","lynn"];
+
+// // todo del
+// console.log(array1);
+// console.log(array2);
+// console.log(array3);
+
+haha = "rex is handsome"
+// todo del
+// console.log(haha);
+// console.log(haha[2]);        
+
+// fruit1 = "apple";
+// fruit2 = 'banana';
+// console.log(fruit1);
+// console.log(fruit2);
+
+a = 50;
+b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+weather = "good";
+if (weather === "good") {
+    console.log("去爬山");
+} else if (weather === "bad") {
+    // todo del
+    console.log("健身");
+}
+
+
+class Person {
+    name = "Rex";
+    age= 17;
+    male = true;
+    constructor () {
+
+    }
+    Function() {
+
+    }
+}
