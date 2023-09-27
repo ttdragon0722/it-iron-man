b45a3eec3305338c52fd7750b41ac30cab1fba73
ttdragon <a0909956502@gmail.com>
2023-09-27 10:54:59 +0800

    MENU.GSAP

--- day11/helloworld.js
+++ day11/helloworld.js
@@ -1,13 +1,14 @@
 // console.log("hello world!!");
 
 array1 = [];
-array2 = [1,2,3];
-array3 = ["rex","vscore","lynn"];
+array2 = [1, 2, 3];
+array3 = ["rex", "viscose", "lynn"];
 
 // // todo del
 // console.log(array1);
 // console.log(array2);
 // console.log(array3);
+console.log(array3[-1]);
 
 haha = "rex is handsome"
 // todo del
@@ -21,27 +22,54 @@ haha = "rex is handsome"
 
 a = 50;
 b = 3;
-
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-
+/** 
 weather = "good";
 if (weather === "good") {
     console.log("去爬山");
 } else if (weather === "bad") {
-    // todo del
     console.log("健身");
+}
+console.log("回家吃飯!!");
+*/
+
+// 註解
+// 這是註解
+
+/**
+ * ## 數字相加
+ * 使用markDown語法撰寫的文本
+ * @param {Int16Array} a A數字
+ * @param {Int16Array} b B數字S
+ * @returns {Int16Array} 結果
+ */
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+// 心情
+// emotion = false;
+// console.log(emotion);
+
+// if ( !emotion ) {
+//     console.log("睡覺");
+//     emotion = !emotion;
+// } else {
+//     console.log("出門");
+// }
+// console.log(emotion);
+
+// 初始值 ; 結束值 ; 低增值 ;
+for (let i=0; i < 10; i++) {
+    // 執行的程式
+    console.log(i);
 }
 
 
 class Person {
     name = "Rex";
-    age= 17;
+    age = 17;
     male = true;
-    constructor () {
+    constructor() {
 
     }
     Function() {
